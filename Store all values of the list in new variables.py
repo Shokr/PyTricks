d1c48a548ea7a64e07af7eb718a62eb7c7715bea
Mohammed Shokr <mohammedshokr2014@gmail.com>
2016-08-10 17:01:57 +0200

    Trick [3]

---
+++ Store all values of the list in new variables.py
@@ -0,0 +1,14 @@
+# Trick [3]: Store all values of the list in new variables
+"""
+a = [1,2,3]
+
+Store all three values of the list in 3 new variables
+"""
+
+a = [1, 2, 3]
+x, y, z = a
+print("x: ",x)
+print("y: ",y)
+print("z: ",z)
+
+print("  ")
