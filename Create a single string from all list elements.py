09ab8566ac6a02e9712cd5ee5b5511724f6ff607
Mohammed Shokr <mohammedshokr2014@gmail.com>
2016-08-10 17:02:10 +0200

    Trick [4]

---
+++ Create a single string from all list elements.py
@@ -0,0 +1,11 @@
+# Trick [4]: Create a single string from all list elements
+"""
+a = ["Fun", "Code", "Daily", "Python"] 
+
+Create a single string from all the elements in list above.
+"""
+
+a = ["Fun", "Code", "Daily", "Python"]
+print (" ".join(a))
+
+print("  ")
