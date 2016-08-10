c6a2d29eeb84fe716c32b80ddec0ca800c0f7dc1
Mohammed Shokr <mohammedshokr2014@gmail.com>
2016-08-10 17:03:02 +0200

    Trick [6]

---
+++ Swap two numbers with one line of code.py
@@ -0,0 +1,19 @@
+# Trick [6]: Swap two numbers with one line of code.
+
+a=7
+b=5
+b, a =a, b
+print(a,b)
+
+print("  ")
+
+
+# Trick [7]: Repeat Strings without using loops
+"""
+print "DailyDailyDailyDaily PythonPythonPythonPythonPython"
+without using loops
+"""
+
+print("Daily"*4+' '+"Python"*5)
+
+print("  ")
