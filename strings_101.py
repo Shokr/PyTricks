75c1262e11410e9180ee10bb2bd47282ef6471df
Shokr <mohammedshokr2014@gmail.com>
2018-10-14 14:41:49 +0200

    string tips which are relatively uncommon and are very useful

---
+++ strings_101.py
@@ -0,0 +1,44 @@
+print("##########Checking if two words are anagrams##################")
+
+# Checking if two words are anagrams
+str1 = "shokr"
+str2 = "shokr"
+
+print(Counter(str1) == Counter(str2))
+print("#################Reverse a String##############")
+
+# Reverse a String
+
+a = 'vivalaegyptehishokrbyagoodluk'
+
+print("Reverse a String with special case of slice step")
+print(a[::-1])
+
+#print("Reverse a String with loop")
+#for char in reversed(a):
+#    print(char)
+
+
+print("Reverse s integer through type Conv & sliceing")
+
+num = 23031994
+print(int(str(num)[::-1]))
+
+print("##########Transpose 2d array##########")
+
+original = [['a','b'],['c','d'],['e','f']]
+transposed = zip(*original)
+print(list(transposed))
+
+print("##########Chained Comparison##########")
+
+b = 6
+print(4 < b < 7)
+print(1 == b < 20)
+
+print("##########Dictionary Get##########")
+
+d = {'a': 1,'b': 2}
+print(d.get('c',3))
+print(d)
+
